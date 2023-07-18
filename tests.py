22131eb7d069c47d9714227bbf202d6d9169ea4d
David Johnson <szuroveczdavid@gmail.com>
2023-07-18 11:55:49 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,14 @@
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+from annotation_service.app import app
 
 client = TestClient(app)
+
+# TC1: Every manadatory fields present
+# TC2: Missing external_project_id
+# TC3: Missing fileset
+# TC4: Missing external_project_id & fileset
 
 
 @pytest.mark.parametrize(
@@ -10,37 +16,53 @@ client = TestClient(app)
     [
         (
             {
-                "external_project_id": "your_external_project_id",
-                "fileset": "your_fileset",
-                "annotation_workflow": {"annotation_provider": "your_annotation_provider"},
-                "annotation_ruleset": "your_annotation_ruleset",
-                "annotation_workflow_config": "your_workflow_config",
+                "external_project_id": "clk6puu0900lo0724azre3hma",
+                "annotation_workflow": {
+                    "annotation_provider": "Labelbox",
+                    "annotation_types": ["string"],
+                    "media_type": "image",
+                },
+                "annotation_ruleset": "string",
+                "annotation_workflow_config": "string",
+                "fileset": "zeiss_file_set://fileset0003",
             },
-            status.HTTP_200_OK,
+            status.HTTP_201_CREATED,
         ),
         (
             {
-                "fileset": "your_fileset",
-                "annotation_workflow": {"annotation_provider": "your_annotation_provider"},
-                "annotation_ruleset": "your_annotation_ruleset",
-                "annotation_workflow_config": "your_workflow_config",
+                "annotation_workflow": {
+                    "annotation_provider": "Labelbox",
+                    "annotation_types": ["string"],
+                    "media_type": "image",
+                },
+                "annotation_ruleset": "string",
+                "annotation_workflow_config": "string",
+                "fileset": "zeiss_file_set://fileset0003",
             },
             status.HTTP_422_UNPROCESSABLE_ENTITY,
         ),
         (
             {
-                "external_project_id": "your_external_project_id",
-                "annotation_workflow": {"annotation_provider": "your_annotation_provider"},
-                "annotation_ruleset": "your_annotation_ruleset",
-                "annotation_workflow_config": "your_workflow_config",
+                "external_project_id": "project_id",
+                "annotation_workflow": {
+                    "annotation_provider": "Labelbox",
+                    "annotation_types": ["string"],
+                    "media_type": "image",
+                },
+                "annotation_ruleset": "string",
+                "annotation_workflow_config": "string",
             },
             status.HTTP_422_UNPROCESSABLE_ENTITY,
         ),
         (
             {
-                "annotation_workflow": {"annotation_provider": "your_annotation_provider"},
-                "annotation_ruleset": "your_annotation_ruleset",
-                "annotation_workflow_config": "your_workflow_config",
+                "annotation_workflow": {
+                    "annotation_provider": "Labelbox",
+                    "annotation_types": ["string"],
+                    "media_type": "image",
+                },
+                "annotation_ruleset": "string",
+                "annotation_workflow_config": "string",
             },
             status.HTTP_422_UNPROCESSABLE_ENTITY,
         ),
@@ -49,7 +71,7 @@ client = TestClient(app)
 def test_create_annotation_request_fileset(payload, expected_status):
     response = client.post("/annotation_request/fileset", json=payload)
     assert response.status_code == expected_status
-    if expected_status == status.HTTP_200_OK:
+    if expected_status == status.HTTP_201_CREATED:
         assert response.json() == {
             "message": "Annotation request created successfully",
             "annotation_request": payload,
